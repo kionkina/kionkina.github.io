50f00d93001b790f47cccfa76d70ffb37a63f550
Karina Ionkina <karina.ionkina15@gmail.com>
2022-12-20 00:51:25 -0500

    Updates

---
+++ precache-manifest.100275900272ec96ac246604918a931e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dec4eaaed95ccdddabe",
+    "url": "/react-gh-pages/static/css/main.d3806e46.chunk.css"
+  },
+  {
+    "revision": "7dec4eaaed95ccdddabe",
+    "url": "/react-gh-pages/static/js/main.7dec4eaa.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-gh-pages/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "844a58ec6fa51f4bc43bf46da7b9d2cd",
+    "url": "/react-gh-pages/index.html"
+  }
+];
