dc6c10d92a601bc528dddb0029b82eaa27dc41a4
Karina Ionkina <karina.ionkina15@gmail.com>
2024-02-14 21:42:57 -0500

    update description

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,180 +1,180 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Karina Ionkina",
-    "role": "full stack developer, artist and musician",
-    "job": "SWE @ Bloomberg",
-    "linkedinId":"karina-ionkina",
-    "skypeid": "Your skypeid",
-    "roleDescription": " In my free time, I like dabbling in the open source world and playing with instruments (and my cat Beaux).",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/karina-ionkina/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/kionkina",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"twitter",
-          "url":"http://twitter.com/carinawithak",
-          "className":"fa fa-twitter"
-        },
-        {
-          "name":"medium",
-          "url":"https://medium.com/@karinaionkina",
-          "className":"fa fa-pencil"
-        }
-      ],
-    "aboutme":"I graduated Hunter College in 2021 with a degree in computer science and Chinese, and am currently a lead engineer on a team at Bloomberg. I'm interested in building and scaling progressive web apps. In an attempt to combine my interest in music and engineering, I'm currently building a react-native app to help musicians/creatives write music with easy access to tuning, chord-progressions, and lyric-writing tools. ",
-    "address":"New York",
-    "website":"https:/kionkina.github.io",
-    "education":[
+  "imagebaseurl":"https://rbhatia46.github.io/",
+  "name": "Karina Ionkina",
+  "role": "full stack developer, artist and musician",
+  "job": "SWE @ Bloomberg",
+  "linkedinId":"karina-ionkina",
+  "skypeid": "Your skypeid",
+  "roleDescription": " In my free time, I like dabbling in the open source world and playing with instruments (and my cat Beaux).",
+  "socialLinks":[
       {
-        "UniversityName":"Macaulay Honors College @ Hunter College",
-        "specialization":"B.A in Computer Science and Chinese",
-        "MonthOfPassing":"",
-        "YearOfPassing":"May 2021",
-        "Achievements":"Valedicatorian | Deadalus Honors CS | Cooperman Business Fellow | Chinese Flagship Program"
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/karina-ionkina/",
+        "className":"fa fa-linkedin"
       },
       {
-        "UniversityName":"Stuyvesant HS",
-        "specialization":"",
-        "MonthOfPassing":"June",
-        "YearOfPassing":"2018",
-        "Achievements":"Girls Who Code President | Tennis Team Captain | JV Volleyball Captain | Honors Society"
+        "name":"github",
+        "url":"http://github.com/kionkina",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"twitter",
+        "url":"http://twitter.com/carinawithak",
+        "className":"fa fa-twitter"
+      },
+      {
+        "name":"medium",
+        "url":"https://medium.com/@karinaionkina",
+        "className":"fa fa-pencil"
       }
     ],
-    "work":[
-      {
-        "CompanyName":"Bloomberg L.P.",
-        "specialization":"Full-stack engineer",
-        "MonthOfLeaving":"Aug",
-        "YearOfLeaving":"2021 - Present",
-        "Achievements":"Lead engineer on full-stack team. Develops features for a post-trade order management system using TypeScript framework, CPP, and SQL. Builds and maintains internal web app with React and Node.js"
-      },
-      {
-        "CompanyName":"CUNY Research Foundation",
-        "specialization":"Web Developer",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018 - Jun 2020",
-        "Achievements":"Buildt and optimized, and hosted responsive Chinese literacy exercises using React, Node. Built the college's Chinese flagsip program website.",
-        "Link": "www.google.com",
-      },
-      {
-        "CompanyName":"Weill Cornell Medicine",
-        "specialization":"Spacial Computing Intern",
-        "MonthOfLeaving":"Jan 2018",
-        "YearOfLeaving":"- Sept 2019",
-        "Achievements":"Worked with a team of professors, clinicians and scientists in the Spatial Computing Lab to improve VR embryo modeling tools by writing C# scripts in Unity."
-      },
-      {
-        "CompanyName":"Launch Math & Science Center",
-        "specialization":"Computer Science Tutor",
-        "MonthOfLeaving":"Sept 20118",
-        "YearOfLeaving":" - Sept 2019",
-        "Achievements":"Taught 3 after-school Python classes at NEST +M and Hunter College Elementary School. Tutored 4 middle and high school students, teaching them computer science and web development fundamentals with pyGame and JavaScript projects."
-      }
-    ],
-    "skillsDescription":"Technical",
-    "skills":[
-      {
-        "skillname":"TypeScript / ES6+",
-        "id": "ts"
-      },
-      {
-        "skillname":"Reactjs",
-        "id": "react"
-      },
-      {
-        "skillname":"CPP",
-        "id": "cpp"
-      },
-      {
-        "skillname":"SQL",
-        "id": "sql"
-      },
-      {
-        "skillname":"React Native (In progress...)",
-        "id": "reactn"
-      },
-      {
-        "skillname":"ios",
-        "id": "ios"
-      },
-      {
-        "skillname":"中文",
-        "id": "mandarin"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"NYC Bike Lane and Collision Anaylysis",
-        "description": "Visualizing the extent to which different kinds of lanes corrrelate to bicycle accidents in nyc.",
-        "imgurl":"images/portfolio/lanes.png",
-        "link": "https://nyc-bikes-and-lanes.herokuapp.com/"
-      },
-      {
-        "name":"Robingood",
-        "description":"A MERN paper trading app.",
-        "imgurl":"images/portfolio/robingood.png",
-        "link": "https://github.com/kionkina/robingood"
-      },
-      {
-        "name":"Meditapp",
-        "description":"An meditation social media app where users can post their meditation recordings.",  
-        "imgurl":"images/portfolio/meditapp.png",
-        "link": "https://github.com/kionkina/meditapp"
-      },
-      {
-        "name":"Kantian Computational Linguistics",
-        "description":"A review of seminal work on linguistics and the structure of language, exploring contemporary solutions, identifying their drawbacks and proposing alternatives to address them.",
-        "imgurl":"images/portfolio/kant.png",
-        "link": "https://dl.acm.org/doi/abs/10.1007/978-3-030-78114-9_17"
-      },
-      {
-        "name":"Stuy Planner",
-        "description":"A classroom management app (flask, js, sqlite). Click for demo :)",
-        "imgurl":"images/portfolio/sp.png",
-        "link": "https://www.youtube.com/watch?v=VoErJulmqds&t=6s"
-      },
-      {
-        "name":"YACMA",
-        "description":"Yet ANOTHER classroom management app. This time with React, Node, Google App Engine, & Firestore -- distributed systems project.",
-        "imgurl":"images/portfolio/Y.png",
-        "link": "https://github.com/boubascript/yacma"
-      },
-      {
-        "name":"PenPal",
-        "description":"An ios app that pairs you with users based on languages and allows you to message them for practice.",
-        "imgurl":"images/portfolio/penpal.png",
-        "link": "https://github.com/kionkina/penpal"
-      },
-      {
-        "name":"RPI Hackathon",
-        "description":"A twitter sentiment analyzer for first responders.",
-        "imgurl":"images/portfolio/rpi.png",
-        "link": "https://devpost.com/software/relief-insights-p6fh9u"
-      },
-    ],
-    "blogs":[
-      {
-      "title": "React Under the Hood",
-      "link": "https://medium.com/@karinaionkina/react-under-the-hood-97e714aba4d3"
-      }
-    ],
-    "Papers":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "aboutme":"I graduated Hunter College in 2021 with a degree in computer science and Chinese, and am currently a lead engineer on a team at Bloomberg. I'm currently pursuing a master's degree at UT Austin in machine learning. I'm interested in building and scaling progressive web apps, as well as applying the theoretical aspects of AI in real-world applications. In my free time, I like playing with instruments and my cat (Beaux).",
+  "address":"New York",
+  "website":"https:/kionkina.github.io",
+  "education":[
+    {
+      "UniversityName":"Macaulay Honors College @ Hunter College",
+      "specialization":"B.A in Computer Science and Chinese",
+      "MonthOfPassing":"",
+      "YearOfPassing":"May 2021",
+      "Achievements":"Valedicatorian | Deadalus Honors CS | Cooperman Business Fellow | Chinese Flagship Program"
+    },
+    {
+      "UniversityName":"Stuyvesant HS",
+      "specialization":"",
+      "MonthOfPassing":"June",
+      "YearOfPassing":"2018",
+      "Achievements":"Girls Who Code President | Tennis Team Captain | JV Volleyball Captain | Honors Society"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Bloomberg L.P.",
+      "specialization":"Full-stack engineer",
+      "MonthOfLeaving":"Aug",
+      "YearOfLeaving":"2021 - Present",
+      "Achievements":"Lead engineer on full-stack team. Develops features for a post-trade order management system using TypeScript framework, CPP, and SQL. Builds and maintains internal web app with React and Node.js"
+    },
+    {
+      "CompanyName":"CUNY Research Foundation",
+      "specialization":"Web Developer",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018 - Jun 2020",
+      "Achievements":"Buildt and optimized, and hosted responsive Chinese literacy exercises using React, Node. Built the college's Chinese flagsip program website.",
+      "Link": "www.google.com",
+    },
+    {
+      "CompanyName":"Weill Cornell Medicine",
+      "specialization":"Spacial Computing Intern",
+      "MonthOfLeaving":"Jan 2018",
+      "YearOfLeaving":"- Sept 2019",
+      "Achievements":"Worked with a team of professors, clinicians and scientists in the Spatial Computing Lab to improve VR embryo modeling tools by writing C# scripts in Unity."
+    },
+    {
+      "CompanyName":"Launch Math & Science Center",
+      "specialization":"Computer Science Tutor",
+      "MonthOfLeaving":"Sept 20118",
+      "YearOfLeaving":" - Sept 2019",
+      "Achievements":"Taught 3 after-school Python classes at NEST +M and Hunter College Elementary School. Tutored 4 middle and high school students, teaching them computer science and web development fundamentals with pyGame and JavaScript projects."
+    }
+  ],
+  "skillsDescription":"Technical",
+  "skills":[
+    {
+      "skillname":"TypeScript / ES6+",
+      "id": "ts"
+    },
+    {
+      "skillname":"Reactjs",
+      "id": "react"
+    },
+    {
+      "skillname":"CPP",
+      "id": "cpp"
+    },
+    {
+      "skillname":"SQL",
+      "id": "sql"
+    },
+    {
+      "skillname":"React Native (In progress...)",
+      "id": "reactn"
+    },
+    {
+      "skillname":"ios",
+      "id": "ios"
+    },
+    {
+      "skillname":"中文",
+      "id": "mandarin"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"NYC Bike Lane and Collision Anaylysis",
+      "description": "Visualizing the extent to which different kinds of lanes corrrelate to bicycle accidents in nyc.",
+      "imgurl":"images/portfolio/lanes.png",
+      "link": "https://nyc-bikes-and-lanes.herokuapp.com/"
+    },
+    {
+      "name":"Robingood",
+      "description":"A MERN paper trading app.",
+      "imgurl":"images/portfolio/robingood.png",
+      "link": "https://github.com/kionkina/robingood"
+    },
+    {
+      "name":"Meditapp",
+      "description":"An meditation social media app where users can post their meditation recordings.",  
+      "imgurl":"images/portfolio/meditapp.png",
+      "link": "https://github.com/kionkina/meditapp"
+    },
+    {
+      "name":"Kantian Computational Linguistics",
+      "description":"A review of seminal work on linguistics and the structure of language, exploring contemporary solutions, identifying their drawbacks and proposing alternatives to address them.",
+      "imgurl":"images/portfolio/kant.png",
+      "link": "https://dl.acm.org/doi/abs/10.1007/978-3-030-78114-9_17"
+    },
+    {
+      "name":"Stuy Planner",
+      "description":"A classroom management app (flask, js, sqlite). Click for demo :)",
+      "imgurl":"images/portfolio/sp.png",
+      "link": "https://www.youtube.com/watch?v=VoErJulmqds&t=6s"
+    },
+    {
+      "name":"YACMA",
+      "description":"Yet ANOTHER classroom management app. This time with React, Node, Google App Engine, & Firestore -- distributed systems project.",
+      "imgurl":"images/portfolio/Y.png",
+      "link": "https://github.com/boubascript/yacma"
+    },
+    {
+      "name":"PenPal",
+      "description":"An ios app that pairs you with users based on languages and allows you to message them for practice.",
+      "imgurl":"images/portfolio/penpal.png",
+      "link": "https://github.com/kionkina/penpal"
+    },
+    {
+      "name":"RPI Hackathon",
+      "description":"A twitter sentiment analyzer for first responders.",
+      "imgurl":"images/portfolio/rpi.png",
+      "link": "https://devpost.com/software/relief-insights-p6fh9u"
+    },
+  ],
+  "blogs":[
+    {
+    "title": "React Under the Hood",
+    "link": "https://medium.com/@karinaionkina/react-under-the-hood-97e714aba4d3"
+    }
+  ],
+  "Papers":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
