5d6a41416dedd2b63a7393b2cdf1081f8681e27c
kionkina <karina.ionkina15@gmail.com>
2026-08-02 14:55:15 -0400

    Updates

---
+++ precache-manifest.e00bb88b2a842c9e74419683ceb4dd65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b9751a05d0e58343ffa",
+    "url": "/static/css/main.647da433.chunk.css"
+  },
+  {
+    "revision": "4b9751a05d0e58343ffa",
+    "url": "/static/js/main.4b9751a0.chunk.js"
+  },
+  {
+    "revision": "55513c49acb0ac5f42b4",
+    "url": "/static/js/1.55513c49.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e448a08a89283db77f6e4877d3d38bb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.958ede7761d2f2ae7119a3694abf9b5c.js"
+  "/precache-manifest.e00bb88b2a842c9e74419683ceb4dd65.js"
 );
 
 workbox.clientsClaim();
